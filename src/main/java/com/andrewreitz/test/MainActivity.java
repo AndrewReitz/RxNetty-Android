80cae6dbbfb8faef7e24bcdc41f3964b84024156
Andrew Reitz <aj.reitz@gmail.com>
2014-09-01 08:18:41 -0500

    Init

---
+++ src/main/java/com/andrewreitz/test/MainActivity.java
@@ -0,0 +1,11 @@
+package com.andrewreitz.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+  @Override public void onCreate(Bundle savedInstance) {
+    super.onCreate(savedInstance);
+    setContentView(R.layout.activity_main);
+  }
+}
